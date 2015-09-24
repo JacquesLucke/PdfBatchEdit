971962bb04b8c34376a6f74d75d7d9ea54c05562
Jacques Lucke <mail@jlucke.com>
2015-09-24 22:39:23 +0200

    fix

--- PdfBatchEdit/AddTextEffect.cs
+++ PdfBatchEdit/AddTextEffect.cs
@@ -5,19 +5,25 @@ namespace PdfBatchEdit
 {
     class AddTextEffect : IPdfEffect
     {
-        public string Text = "Hello World";
+        public string text = "Hello World";
 
         public void ApplyEffect(PdfDocument document)
         {
             PdfPage page = document.Pages[0];
             XGraphics gfx = XGraphics.FromPdfPage(page, XGraphicsPdfPageOptions.Append);
             XFont font = new XFont("Verdana", 15, XFontStyle.Regular);
-            gfx.DrawString(Text, font, XBrushes.Red, new XPoint(300, 200));
+            gfx.DrawString(text, font, XBrushes.Red, new XPoint(300, 200));
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
         }
 
         public override string ToString()
         {
-            return "Add Text: " + Text;
+            return "Add Text: " + text;
         }
     }
 }
