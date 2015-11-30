f97d4d6e372abce373d29c3a5301ba6cba05dd69
Jacques Lucke <mail@jlucke.com>
2015-11-30 18:51:36 +0100

    preprocess some pdf files with the itextsharp library

--- PdfBatchEdit/SourceFile.cs
+++ PdfBatchEdit/SourceFile.cs
@@ -1,5 +1,6 @@
 ﻿using PdfSharp.Pdf;
 using PdfSharp.Pdf.IO;
+using System.IO;
 
 namespace PdfBatchEdit
 {
@@ -9,7 +10,32 @@ namespace PdfBatchEdit
 
         public PdfDocument Load()
         {
-            return PdfReader.Open(Path, PdfDocumentOpenMode.Modify);
+            PdfDocument document = null;
+            try
+            {
+                return PdfReader.Open(Path, PdfDocumentOpenMode.Modify);
+            }
+            catch (PdfReaderException)
+            {
+                FileStream fs = new FileStream(Path, FileMode.Open, FileAccess.Read);
+                int len = (int)fs.Length;
+                byte[] fileArray = new byte[len];
+                fs.Read(fileArray, 0, len);
+                fs.Close();
+
+                MemoryStream sourceStream = new MemoryStream(fileArray);
+                sourceStream.Position = 0;
+                MemoryStream outputStream = new MemoryStream();
+                iTextSharp.text.pdf.PdfReader reader = new iTextSharp.text.pdf.PdfReader(sourceStream);
+                iTextSharp.text.pdf.PdfStamper pdfStamper = new iTextSharp.text.pdf.PdfStamper(reader, outputStream);
+                pdfStamper.FormFlattening = true;
+                pdfStamper.Writer.SetPdfVersion(iTextSharp.text.pdf.PdfWriter.PDF_VERSION_1_4);
+                pdfStamper.Writer.CloseStream = false;
+                pdfStamper.Close();
+
+                document = PdfReader.Open(outputStream, PdfDocumentOpenMode.Modify);
+            }
+            return document;
         }
     }
 }
