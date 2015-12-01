c25edf2661929ee35d8254dcfefb6f533fa5d67a
Jacques Lucke <mail@jlucke.com>
2015-12-01 19:57:10 +0100

    implement pdf loader

---
+++ PdfBatchEdit/PdfLoader.cs
@@ -0,0 +1,49 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System.IO;
+
+namespace PdfBatchEdit
+{
+    public class PdfLoader
+    {
+        public static PdfDocument Load(string path)
+        {
+            try
+            {
+                return PdfReader.Open(path, PdfDocumentOpenMode.Modify);
+            }
+            catch
+            {
+                MemoryStream pdfStream = MemoryStreamFromPath(path);
+                MemoryStream convertedPdfStream = GetStreamWithConvertedPdf(pdfStream);
+                return PdfReader.Open(convertedPdfStream, PdfDocumentOpenMode.Modify);
+            }
+        }
+
+        private static MemoryStream GetStreamWithConvertedPdf(MemoryStream source)
+        {
+            MemoryStream output = new MemoryStream();
+            iTextSharp.text.pdf.PdfReader reader = new iTextSharp.text.pdf.PdfReader(source);
+            iTextSharp.text.pdf.PdfStamper pdfStamper = new iTextSharp.text.pdf.PdfStamper(reader, output);
+            pdfStamper.FormFlattening = true;
+            pdfStamper.Writer.SetPdfVersion(iTextSharp.text.pdf.PdfWriter.PDF_VERSION_1_4);
+            pdfStamper.Writer.CloseStream = false;
+            pdfStamper.Close();
+
+            return output;
+        }
+
+        private static MemoryStream MemoryStreamFromPath(string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            int len = (int)fs.Length;
+            byte[] array = new byte[len];
+            fs.Read(array, 0, len);
+            fs.Close();
+
+            MemoryStream ms = new MemoryStream(array);
+            ms.Position = 0;
+            return ms;
+        }
+    }
+}
--- PdfBatchEdit/SourceFile.cs
+++ PdfBatchEdit/SourceFile.cs
@@ -10,32 +10,7 @@ namespace PdfBatchEdit
 
         public PdfDocument Load()
         {
-            PdfDocument document = null;
-            try
-            {
-                return PdfReader.Open(Path, PdfDocumentOpenMode.Modify);
-            }
-            catch (PdfReaderException)
-            {
-                FileStream fs = new FileStream(Path, FileMode.Open, FileAccess.Read);
-                int len = (int)fs.Length;
-                byte[] fileArray = new byte[len];
-                fs.Read(fileArray, 0, len);
-                fs.Close();
-
-                MemoryStream sourceStream = new MemoryStream(fileArray);
-                sourceStream.Position = 0;
-                MemoryStream outputStream = new MemoryStream();
-                iTextSharp.text.pdf.PdfReader reader = new iTextSharp.text.pdf.PdfReader(sourceStream);
-                iTextSharp.text.pdf.PdfStamper pdfStamper = new iTextSharp.text.pdf.PdfStamper(reader, outputStream);
-                pdfStamper.FormFlattening = true;
-                pdfStamper.Writer.SetPdfVersion(iTextSharp.text.pdf.PdfWriter.PDF_VERSION_1_4);
-                pdfStamper.Writer.CloseStream = false;
-                pdfStamper.Close();
-
-                document = PdfReader.Open(outputStream, PdfDocumentOpenMode.Modify);
-            }
-            return document;
+            return PdfLoader.Load(Path);
         }
     }
 }
